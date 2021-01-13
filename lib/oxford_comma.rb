92ad2267236fa677803f204cc953c0a5fd7f5518
figo5530 <figooooo0221@gmail.com>
2021-01-12 20:31:17 -0500

    commit tree 4686579e09ce5f7b84618b0e328d2f54b072d341

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    if array.size ==1
+        return array.join
+    elsif array.size ==2
+        return array.join(" and ")
+    else
+        # str = array.join(", ")
+        # str.reverse!
+        # index = str.index(",")
+        # str.insert(index,"dna ")
+        # return str.reverse!
+        array[-1].insert(0,"and ")
+        return array.join(", ")
+    end
 end
+
+puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
